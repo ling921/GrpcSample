32195b2258e11829e3e0a89e3bc05005fa704c49
lingjing <lingjing@jxxhy.cn>
2021-08-05 14:42:32 +0800

    添加项目文件。

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = await client.SayHelloAsync(
+                new HelloRequest { Name = "World" });
+
+            Console.WriteLine(response.Message);
+
+            Console.ReadKey();
+        }
+    }
+}
